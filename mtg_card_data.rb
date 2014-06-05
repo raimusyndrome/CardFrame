1f0b8b7decbe1d7b6149220a692be26dbad93add
raimusyndrome <raimusyndrome@example.com>
2014-06-05 18:08:43 +0900

    カードデータ読み込み処理追加

---
+++ mtg_card_data.rb
@@ -0,0 +1,142 @@
+require "CSV"
+
+class CardData
+    module ColNo
+        COST = 2
+
+        NAME = 24
+        SPTYPE = 6
+        TYPE = 7
+        SUBTYPE = 8
+        TEXT = 26
+
+        COLOR = 5
+        POWER = 10
+        TOUGHNESS = 11
+        LOYALTY = 12
+
+        REV_NAME = 32 
+        REV_SPTYPE = 16
+        REV_TYPE = 17
+        REV_SUBTYPE = 18
+        REV_TEXT = 34
+        REV_COLOR = 15
+        REV_POWER = 20
+        REV_TOUGHNESS = 21
+        REV_LOYALTY = 22
+
+        EN_NAME = 23
+        EN_TEXT = 25
+
+        CARDNO = 30
+        CARDSET = 40
+        RARITY = 41
+    end
+
+    def initialize
+        @face = Side.new(SideType::FACE)
+        @back = Side.new(SideType::BACK)
+    end
+
+    def set row
+       @cost = row[ColNo::COST]; 
+       @face.set(row);
+       @back.set(row);
+       @cardNo = row[ColNo::CARDNO];
+       @cardSet = row[ColNo::CARDSET];
+       @rarity = row[ColNo::RARITY];
+    end
+
+    def to_s
+        str = @face.name + "\n"
+        str += @face.color + " : " + @cost + "\n"
+        str += @face.getType
+        str += "\n"
+        str += @face.getParam
+        str += "\n"
+        str += @face.text + "\n"
+        str += @rarity + " / " + @cardSet + "\n"
+
+        return str;
+    end
+
+    module SideType
+        FACE = 0
+        BACK = 1
+    end
+
+    class Side
+        attr_reader :name, :color, :text
+
+        def initialize type
+            @type = type
+        end
+
+        def set row
+            if @type == SideType::FACE then
+                getFaceData(row)
+            else
+                getBackData(row)
+            end
+        end
+
+        def getFaceData row
+            @name       = row[ColNo::NAME];
+            @spType     = row[ColNo::SPTYPE]
+            @type       = row[ColNo::TYPE]
+            @subType    = row[ColNo::SUBTYPE]
+            @text       = row[ColNo::TEXT]
+            @color      = row[ColNo::COLOR]
+            @power      = row[ColNo::POWER]
+            @toughness  = row[ColNo::TOUGHNESS]
+            @loyalty    = row[ColNo::LOYALTY]
+        end
+
+        def getBackData row
+            @name = row[ColNo::REV_NAME];
+        end
+
+        def getType
+            if @spType.nil? then
+                str = @type
+            else 
+                str = @spType + @type
+            end
+            if !@subType.nil? then
+                str += " - " + @subType
+            end
+            return str;
+        end
+
+        def getParam
+            if @loyalty.nil? then
+                str = @power.to_s + " / " + @toughness.to_s
+            else
+                str = @loyalty.to_s
+            end
+            return str;
+        end
+
+    end
+end
+
+def MTGParser file
+    cardlist = Array.new;
+    open( file, "r:Shift_JIS:UTF-8", undef: :replace) do |f|
+        CSV.new( f, headers: true, converters: :numeric ).each do |row|
+            card = CardData.new
+
+            card.set(row);
+
+            cardlist << card;
+        end
+    end
+
+    return cardlist
+end
+
+if __FILE__ == $0 then
+    list = MTGParser "THS_BNG.csv"
+    puts list[0].to_s
+end
+
